e2ca313d6d4cddec05edc1886f3e857a3100d47f
FenderShredder512 <ejmetall@gmail.com>
2025-08-01 16:16:37 -0500

    Added technically functional form to add expenses

--- budget/app/routes/add.tsx
+++ budget/app/routes/add.tsx
@@ -1,4 +1,6 @@
+import * as React from "react";
 import Button from "@mui/material/Button";
+import { TextField } from "@mui/material";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -10,29 +12,39 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Add() {
   return (
-    <Button variant="contained" onClick={addData}>
-      Add test data
-    </Button>
+    <form action={addData}>
+      <Button type="submit" variant="contained">
+        Add transaction(s)
+      </Button>
+      <section>
+        <TextField name="Date" label="Date" helperText="MM-dd-yyyy"></TextField>
+        <TextField name="Amount" label="Amount" helperText="$USD"></TextField>
+        <TextField
+          name="Institution"
+          label="Institution"
+          helperText=" "
+        ></TextField>
+        <TextField name="Category" label="Category" helperText=" "></TextField>
+        <TextField
+          name="Subcategory"
+          label="Subcategory"
+          helperText=" "
+        ></TextField>
+        <TextField name="Comment" label="Comment" helperText=" "></TextField>
+      </section>
+    </form>
   );
 }
 
-async function addData() {
+async function addData(formData: FormData) {
   const url = `http://localhost:8080/expense/add`;
-
-  let testExpense = [
-    {
-      Date: "01-01-2000",
-      Amount: "$666",
-      Institution: "First National Test",
-      Category: "Other",
-      Comment: "Test",
-    },
-  ];
+  const expenses = Object.fromEntries(formData.entries());
+  console.log("Test: " + JSON.stringify(expenses));
 
   try {
     const response = await fetch(url, {
       method: "POST",
-      body: JSON.stringify(testExpense),
+      body: JSON.stringify(expenses),
     });
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
