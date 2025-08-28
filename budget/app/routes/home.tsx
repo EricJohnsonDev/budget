49a8122da789578dd3a462e92962e1bf2098c5d8
FenderShredder512 <ejmetall@gmail.com>
2025-08-28 10:05:57 -0500

    Updates dashboard to calculate total expenses over given date range

--- budget/app/routes/home.tsx
+++ budget/app/routes/home.tsx
@@ -1,4 +1,10 @@
 import type { Route } from "./+types/home";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import type { Tx_expenses } from "~/partials/models";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,5 +14,76 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return "Home sweet home";
+  const [sumExpenses, setSumExpenses] = React.useState(0);
+
+  const updateData = async (formData: FormData) => {
+    const params = new URLSearchParams();
+    params.append("start", formData.get("startDate") as string);
+    params.append("end", formData.get("endDate") as string);
+
+    const url = `http://localhost:8080/expense/date?${params}`;
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const json = await response.json();
+      refreshDashboard(json);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const calcSumExpenses = (expensesIn: Tx_expenses[]) => {
+    let result = 0;
+
+    for (let i = 0; i < expensesIn.length; i++) {
+      let amount = parseFloat(formatAmount(expensesIn[i].Amount));
+      result += amount;
+    }
+
+    setSumExpenses(result);
+  };
+
+  const formatAmount = (amount: string) => {
+    let formattedAmount = amount
+      .replace(")", "")
+      .replace("(", "-")
+      .replace("$", "")
+      .replace(",", "");
+    console.log(amount, " -> ", formattedAmount);
+
+    return formattedAmount;
+  };
+
+  const refreshDashboard = (expensesIn: Tx_expenses[]) => {
+    calcSumExpenses(expensesIn);
+  };
+
+  return (
+    <Box>
+      <h1>Dashboard</h1>
+      <form action={updateData}>
+        <TextField
+          name="startDate"
+          label="Start Date"
+          helperText="MM-dd-yyyy"
+        ></TextField>
+        <TextField
+          name="endDate"
+          label="End Date"
+          helperText="MM-dd-yyyy"
+        ></TextField>
+        <Button
+          type="submit"
+          variant="contained"
+          className="h-16"
+          startIcon={<RefreshIcon />}
+        ></Button>
+      </form>
+      <p>Total Expenses: {sumExpenses}</p>
+    </Box>
+  );
 }
