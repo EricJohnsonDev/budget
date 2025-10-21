5bd834ee6e6e2fd49f73f575646ab8bb40b74928
FenderShredder512 <ejmetall@gmail.com>
2025-10-21 11:53:42 -0500

    adds test coverage for toolbar

--- budget/app/partials/test/toolbar.test.tsx
+++ budget/app/partials/test/toolbar.test.tsx
@@ -1,9 +1,21 @@
-import { render, screen } from "@testing-library/react";
-import { expect, test } from "vitest";
+import { render, screen, cleanup, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { expect, test, afterEach } from "vitest";
 import BudgetToolbar from "../toolbar";
+import Home from "../../routes/home";
+
+afterEach(() => {
+  cleanup();
+});
+
+const setMobileResolution = () => {
+  window.innerWidth = 430;
+  window.dispatchEvent(new Event("resize"));
+};
 
 test("Toolbar header is visible on mobile resolution", () => {
   render(<BudgetToolbar></BudgetToolbar>);
+
   const toolbar = screen.queryByText("EDJ Dev Budget");
   expect(toolbar).not.toBeNull();
 });
@@ -19,9 +31,8 @@ test("Sidebar is visible on desktop resolution", () => {
 });
 
 test("Sidebar is hidden by default on mobile resolution", () => {
-  window.innerWidth = 430;
-  window.dispatchEvent(new Event("resize"));
   render(<BudgetToolbar></BudgetToolbar>);
+  setMobileResolution();
 
   const desktopDrawer = screen.getByTestId("desktopDrawer");
   const mobileDrawer = screen.getByTestId("mobileDrawer");
@@ -30,10 +41,38 @@ test("Sidebar is hidden by default on mobile resolution", () => {
   expect(window.getComputedStyle(mobileDrawer).visibility).toBe("hidden");
 });
 
-test.skip("Sidebar can be expanded on mobile resolution", () => {
+test("Sidebar can be expanded and collapsed with menu icon on mobile resolution", async () => {
+  const user = userEvent.setup();
   render(<BudgetToolbar></BudgetToolbar>);
+  setMobileResolution();
+
+  const mobileDrawer = screen.getByTestId("mobileDrawer");
+  const navMenuButton = screen.getByLabelText("open drawer");
+
+  await user.click(navMenuButton);
+  expect(window.getComputedStyle(mobileDrawer).visibility).toBe("visible");
+
+  await user.click(navMenuButton);
+  waitFor(() =>
+    expect(window.getComputedStyle(mobileDrawer).visibility).toBe("hidden"),
+  );
 });
 
-test.skip("Sidebar can be collapsed on mobile resolution", () => {
+test("Sidebar can be collapsed when background screen is clicked", async () => {
+  const user = userEvent.setup();
   render(<BudgetToolbar></BudgetToolbar>);
+  setMobileResolution();
+
+  const mobileDrawer = screen.getByTestId("mobileDrawer");
+  const navMenuButton = screen.getByLabelText("open drawer");
+
+  await user.click(navMenuButton);
+  expect(window.getComputedStyle(mobileDrawer).visibility).toBe("visible");
+
+  // TODO investigate clicking off the toolbar/sidebar for this functionality
+  // This "works", but doesn't actually cover a mobile use case
+  await user.keyboard("{Esc}");
+  waitFor(() =>
+    expect(window.getComputedStyle(mobileDrawer).visibility).toBe("hidden"),
+  );
 });
