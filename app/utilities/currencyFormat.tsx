95162a8d56969dda61354fb63470573b8d420a34
FenderShredder512 <ejmetall@gmail.com>
2025-11-04 08:24:38 -0600

    fix: currency formatting when amount less than 1

--- app/utilities/currencyFormat.tsx
+++ app/utilities/currencyFormat.tsx
@@ -2,6 +2,9 @@ export const formatCentAmount = (centsIn: number): string => {
   let dollars = centsIn.toString().slice(0, -2);
   let cents = centsIn.toString().slice(-2);
 
+  // If amt < 1.00, ensure dollars is formatted w/ 0
+  dollars = dollars.length == 0 ? "0" : dollars
+
   return `$${dollars}.${cents}`;
 };
 
